9383b2d551dd76b9cd8673b8586f722cd8e71fd0
realkoder <alexanderbtcc@gmail.com>
2025-02-12 13:47:40 +0100

    feat: rabbitmq configured. Rabbitmq conf establshed for both bakcend services

--- RabbitMQ_Example/product-service/src/index.ts
+++ RabbitMQ_Example/product-service/src/index.ts
@@ -1,11 +1,51 @@
 import express from "express";
 import cors from 'cors';
+import amqp from "amqplib";
 
 const app = express();
 const PORT = 8080;
 
 app.use(cors());
 app.use(express.json());
+
+const queue = "product_inventory";
+const text = {
+  id: "macbook",
+  text: "This is a sample message to send receiver to check the ordered Item Availablility",
+};
+
+let connection: amqp.Connection;
+let channel: amqp.Channel;
+
+async function connectToRabbitMQ() {
+  try {
+    connection = await amqp.connect("amqp://guest:guest@rabbitmq:5672/");
+    channel = await connection.createChannel();
+    await channel.assertQueue(queue, { durable: false });
+    console.log("Connected to RabbitMQ and created channel");
+  } catch (err) {
+    // TODO Very simple reconnect strategy here - could do weird stuff be aware!
+    setTimeout(connectToRabbitMQ, 5000);
+    console.warn(err);
+  }
+}
+
+async function publishMessage(message: { id: string, text: string }) {
+  try {
+    channel.sendToQueue(queue, Buffer.from(JSON.stringify(message)));
+    console.log(" [x] Sent '%s'", message);
+  } catch (err) {
+    console.warn(err);
+  }
+}
+
+connectToRabbitMQ();
+
+app.post("/publish", (req, res) => {
+  // const message = req.body;
+  publishMessage(text);
+  res.send({ data: "Message published to RabbitMQ" });
+});
 
 app.get("/", (req, res) => {
   res.send({ data: "hello there from PRODUCT_SERVICE" });
--- RabbitMQ_Example/user-service/src/index.ts
+++ RabbitMQ_Example/user-service/src/index.ts
@@ -1,11 +1,37 @@
 import express from "express";
 import cors from 'cors';
+import amqp from "amqplib";
 
 const app = express();
 const PORT = 9090;
 
 app.use(cors());
 app.use(express.json());
+
+const queue = "product_inventory";
+
+let connection: amqp.Connection;
+let channel: amqp.Channel;
+
+async function connectToRabbitMQ() {
+  try {
+    connection = await amqp.connect("amqp://guest:guest@rabbitmq:5672/");
+    channel = await connection.createChannel();
+    await channel.assertQueue(queue, { durable: false });
+    console.log("Connected to RabbitMQ and created channel");
+
+    channel.consume(queue, async (msg) => {
+      const messageObject = JSON.parse(msg.content.toString());
+      console.log("LOOKK", messageObject);
+    })
+  } catch (err) {
+    // TODO Very simple reconnect strategy here - could do weird stuff be aware!
+    setTimeout(connectToRabbitMQ, 5000);
+    console.warn(err);
+  }
+}
+
+connectToRabbitMQ();
 
 app.get("/", (req, res) => {
   res.send({ data: "hello there from USER_SERVICE" });
